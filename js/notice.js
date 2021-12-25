184bf9d8f3e65ce4564941a88fe823df0ee21487
Ubuntu <PMADMIN@VueTest.3rcbd52shs3u5esdk1b0jecxob.cx.internal.cloudapp.net>
2021-12-25 07:04:25 +0000

    lso200526

--- js/notice.js
+++ js/notice.js
@@ -1,5 +1,5 @@
 // Paste New Title Here
-let newTitle = "Title of Memo"
+let newTitle = "NOTICE OF LAW SOCIETY TRUSTEESHIP"
 // Paste New Warning Here
 let newWarning = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 // Paste New helpful Message for support here
